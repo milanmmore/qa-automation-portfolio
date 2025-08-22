9b05ec618f89d9b60d560e7ef0c76d966552477e
milanmmore2025 <milanmmore2025@gmail.com>
2025-08-22 12:29:08 +0530

    Clean up tracked files now ignored by .gitignore

--- Extras/Cypress_Starter_Projects/cypress-ui-sample/cypress/e2e/google.cy.js
+++
@@ -1,7 +0,0 @@
-describe('Google Search', () => {
-  it('should search for Cypress', () => {
-    cy.visit('https://google.com')
-    cy.get('input[name="q"]').type('Cypress.io{enter}')
-    cy.contains('Cypress.io')
-  })
-})
